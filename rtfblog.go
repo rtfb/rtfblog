2d43c7fcfd7a58294c4089e7fd62eb9aba59b6ea
Vytautas Saltenis <Vytautas.Shaltenis@gmail.com>
2012-12-16 19:01:07 +0200

    Add code to load simple test entries
    Load text files with one-liner for a post title and the rest of the file
    as the body of a post to be treated as markdown. Treat those files as
    blog posts and pass them to renderer.

--- rtfblog.go
+++ rtfblog.go
@@ -7,6 +7,8 @@ import (
     "io/ioutil"
     "log"
     "os"
+    "path/filepath"
+    "strings"
 )
 
 type Entry struct {
@@ -14,12 +16,42 @@ type Entry struct {
     Body     string
 }
 
+func readTextEntry(filename string) (entry *Entry, err error) {
+    f, err := os.Open(filename)
+    if err != nil {
+        return nil, err
+    }
+    b, err := ioutil.ReadAll(f)
+    if err != nil {
+        return nil, err
+    }
+
+    lines := strings.Split(string(b), "\n")
+    entry = new(Entry)
+    entry.Title = lines[0]
+    text := strings.Join(lines[2:], "\n")
+    entry.Body = string(blackfriday.MarkdownCommon([]byte(text)))
+    return
+}
+
+func readTextEntries(root string) (entries []*Entry, err error) {
+    filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        if strings.ToLower(filepath.Ext(path)) != ".txt" {
+            return nil
+        }
+        entry, _ := readTextEntry(path)
+        if entry == nil {
+            return nil
+        }
+        entries = append(entries, entry)
+        return nil
+    })
+    return
+}
+
 func handler(ctx *web.Context, path string) {
     if path == "" {
-        var data = []Entry {
-            {"Title1", "Body 1"},
-            {"Title2", "Body 2"},
-        }
+        data, _ := readTextEntries("testdata")
         html := mustache.RenderFile("hello.mustache",
             map[string]interface{}{
                 "entries": data})
