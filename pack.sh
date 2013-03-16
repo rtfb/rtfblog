93b7ecb9163c1845ae4a2578411811be2a86655b
Vytautas Šaltenis <Vytautas.Shaltenis@gmail.com>
2013-03-16 21:11:17 +0200

    Add a script to pack a tarball

---
+++ pack.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+package=./package
+
+if [ -d $package ] ; then
+    rm -r $package
+fi
+
+mkdir -p $package/dbtool
+
+cp rtfblog.go $package
+cp rtfblog_test.go $package
+cp dbtool/dbtool.go $package/dbtool
+cp dbtool/b2e-import.go $package/dbtool
+cp Makefile $package
+cp sample-server.conf $package
+cp -r static $package
+cp -r tmpl $package
+cp stuff/images/* $package/static/
+
+./dbtool/dbtool -db=./testdata/db.conf -src=./testdata/legacy-db.conf -notest
+cp ./testdata/foo.db $package/main.db
+
+tar cvf package.tar.gz $package
