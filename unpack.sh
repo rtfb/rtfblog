fe7a39f85246bd490d82301629c9c52f356369ca
Vytautas Šaltenis <vytas@rtfb.lt>
2013-06-29 21:47:26 +0300

    Fix a bug in unpack.sh
    Remove the last package-latest right before overwriting it with the new
    latest.

--- unpack.sh
+++ unpack.sh
@@ -7,6 +7,11 @@ fi
 mkdir -p package-tmp
 tar xzvf package.tar.gz -C package-tmp
 killall rtfblog
+
+if [ -d package-last ]; then
+    rm -rf package-last
+fi
+
 mv package package-last
 mv package-tmp/package ./package
 rmdir package-tmp
