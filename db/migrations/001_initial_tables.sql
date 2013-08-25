e599122b9831cd5299d73fc1bc80d6400e93eeef
Vytautas Šaltenis <vytas@rtfb.lt>
2013-08-25 18:43:01 +0300

    Migrate Goose stuff to Postgres

--- db/migrations/001_initial_tables.sql
+++ db/migrations/001_initial_tables.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 create table tag (
-    id integer not null primary key,
+    id serial primary key,
     name text,
     url text
 );
 create table author (
-    id integer not null primary key,
+    id serial primary key,
     disp_name text,
     salt text,
     passwd text,
@@ -14,37 +14,37 @@ create table author (
     www text
 );
 create table post (
-    id integer not null primary key,
+    id serial primary key,
     author_id integer not null references author(id) on delete cascade on update cascade,
     title text,
-    date long,
+    date bigint,
     url text,
     body text
 );
 create table tagmap (
-    id integer not null primary key,
+    id serial primary key,
     tag_id integer not null references tag(id) on delete cascade on update cascade,
     post_id integer not null references post(id) on delete cascade on update cascade
 );
 create table commenter (
-    id integer not null primary key,
+    id serial primary key,
     name text,
     email text,
     www text,
     ip text
 );
 create table comment (
-    id integer not null primary key,
+    id serial primary key,
     commenter_id integer not null references commenter(id) on delete cascade on update cascade,
     post_id integer not null references post(id) on delete cascade on update cascade,
-    timestamp long,
+    timestamp bigint,
     body text
 );
 
 -- +goose Down
-drop table tag;
-drop table author;
-drop table post;
-drop table tagmap;
-drop table commenter;
 drop table comment;
+drop table tagmap;
+drop table tag;
+drop table post;
+drop table author;
+drop table commenter;
