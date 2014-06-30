81b9cf0a325292a716076e1fac676f89ba2bca60
Vytautas Saltenis <vytas@rtfb.lt>
2014-06-30 20:07:22 +0300

    Fixup coverage script

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -4,6 +4,8 @@ git archive HEAD -o git-arch-for-deploy.tar.gz
 dest=$GOPATH/src/rtfblog
 mkdir -p $dest
 tar xzvf git-arch-for-deploy.tar.gz -C $dest
+make version
+cp src/version.go $dest/src
 pushd $dest
 go test -coverprofile=coverage.out ./src/...
 go tool cover -html=coverage.out
