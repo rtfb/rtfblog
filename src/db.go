bdd4274dbc9ff8afcffc4eb0d2d5acfe73e17976
Vytautas Saltenis <vytas@rtfb.lt>
2014-08-07 19:15:34 +0300

    Move all markdown-related stuff to a separate file

--- src/db.go
+++ src/db.go
@@ -3,11 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"html/template"
 	"time"
-
-	"github.com/microcosm-cc/bluemonday"
-	"github.com/russross/blackfriday"
 )
 
 type Data interface {
@@ -38,35 +34,6 @@ type DbData struct {
 	db            *sql.DB
 	tx            *sql.Tx
 	includeHidden bool
-}
-
-func mdToHTML(md string) []byte {
-	htmlFlags := 0
-	htmlFlags |= blackfriday.HTML_USE_XHTML
-	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
-	htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
-	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
-	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
-	extensions := 0
-	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
-	extensions |= blackfriday.EXTENSION_TABLES
-	extensions |= blackfriday.EXTENSION_FENCED_CODE
-	extensions |= blackfriday.EXTENSION_AUTOLINK
-	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
-	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
-	extensions |= blackfriday.EXTENSION_HEADER_IDS
-	return blackfriday.Markdown([]byte(md), renderer, extensions)
-}
-
-func sanitizeTrustedHTML(html []byte) template.HTML {
-	p := bluemonday.UGCPolicy()
-	p.RequireNoFollowOnLinks(false)
-	p.AllowAttrs("alt").OnElements("img")
-	return template.HTML(p.Sanitize(string(html)))
-}
-
-func sanitizeHTML(html []byte) template.HTML {
-	return template.HTML(bluemonday.UGCPolicy().Sanitize(string(html)))
 }
 
 func (dd *DbData) hiddenPosts(flag bool) {
