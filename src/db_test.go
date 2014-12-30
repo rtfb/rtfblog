1a90a542454346d96298d4fc1f7cd0e736bb935e
Vytautas Šaltenis <vytas@rtfb.lt>
2014-12-30 19:29:14 +0200

    Some more tests for post queries

--- src/db_test.go
+++ src/db_test.go
@@ -119,6 +119,69 @@ func testTags(t *testing.T) {
 	}
 }
 
+func testNumPosts(t *testing.T) {
+	// Insert couple more posts
+	data.begin()
+	id, err := data.insertPost(1, &Entry{
+		EntryLink: EntryLink{
+			Title:  "title2",
+			URL:    "url2",
+			Hidden: false,
+		},
+		Author:  "me",
+		Date:    "2014-12-30",
+		RawBody: "*markdown 2*",
+	})
+	if err != nil || id != 2 {
+		data.rollback()
+		t.Fatalf("Failed to insert post, err = %s", err.Error())
+	}
+	id, err = data.insertPost(1, &Entry{
+		EntryLink: EntryLink{
+			Title:  "title3",
+			URL:    "url3",
+			Hidden: false,
+		},
+		Author:  "me",
+		Date:    "2014-12-30",
+		RawBody: "*markdown 3*",
+	})
+	if err != nil || id != 3 {
+		data.rollback()
+		t.Fatalf("Failed to insert post, err = %s", err.Error())
+	}
+	data.commit()
+
+	// Now test a few methods
+	numPosts := data.numPosts()
+	if numPosts != 3 {
+		t.Errorf("Wrong numPosts: expected %d, but got %d", 3, numPosts)
+	}
+
+	allPosts := data.posts(-1, 0)
+	if len(allPosts) != 3 {
+		t.Errorf("Wrong len(allPosts): expected %d, but got %d", 3, len(allPosts))
+	}
+
+	secondPost := data.posts(1, 1)
+	if len(secondPost) != 1 {
+		t.Errorf("Wrong len(secondPost): expected %d, but got %d", 1, len(secondPost))
+	}
+
+	titles := data.titles(-1)
+	if len(titles) != 3 {
+		t.Errorf("Wrong len(titles): expected %d, but got %d", 3, len(titles))
+	}
+
+	firstTitle := data.titles(1)
+	if len(firstTitle) != 1 {
+		t.Errorf("Wrong len(firstTitle): expected %d, but got %d", 1, len(firstTitle))
+	}
+	if firstTitle[0].Title != "title" {
+		t.Errorf("Wrong firstTitle.Title: expected %q, but got %q", "title", firstTitle[0].Title)
+	}
+}
+
 func TestDB(t *testing.T) {
 	if realDB == nil {
 		return
@@ -133,4 +196,5 @@ func TestDB(t *testing.T) {
 	testPost(t)
 	testUpdateTags(t)
 	testTags(t)
+	testNumPosts(t)
 }
