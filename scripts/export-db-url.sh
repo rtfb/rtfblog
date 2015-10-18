d1c79e4ab2f45232a5ae73cc1318fc24d8ece64c
Vytautas Šaltenis <vytas@rtfb.lt>
2015-10-18 18:09:15 +0300

    Parameterize driver in Goose config

--- scripts/export-db-url.sh
+++ scripts/export-db-url.sh
@@ -2,3 +2,4 @@
 
 export RTFBLOG_DB_TEST_URL="user=tstusr dbname=tstdb password=tstpwd"
 export RTFBLOG_DB_STAGING_TEST_URL="user=tstusr dbname=stagdb password=tstpwd"
+export RTFBLOG_DB_DRIVER=postgres
