ef7830c6e269647820ec8ed4fad50b9e85aebd55
Vytautas Šaltenis <vytas@rtfb.lt>
2015-10-18 18:15:53 +0300

    Port over Goose migrations to sqlite dialect

--- db/migrations/004_simplify_tag_table.sql
+++ db/migrations/004_simplify_tag_table.sql
@@ -1,7 +1,28 @@
 -- +goose Up
-alter table tag drop column name;
-alter table tag rename column url to tag;
+alter table tag rename to tmp_tag;
+
+create table tag (
+    id serial primary key,
+    tag text
+);
+
+insert into tag(id, tag)
+select id, url
+from tmp_tag;
+
+drop table tmp_tag;
 
 -- +goose Down
-alter table tag add column name text;
-alter table tag rename column tag to url;
+alter table tag rename to tmp_tag;
+
+create table tag (
+    id serial primary key,
+    name text,
+    url text
+);
+
+insert into tag(id, name, url)
+select id, tag, tag
+from tmp_tag;
+
+drop table tmp_tag;
