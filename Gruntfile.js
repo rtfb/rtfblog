9af7edc508d5db894a3583d07b6e0b2a527e46a4
Vytautas Šaltenis <vytas@rtfb.lt>
2023-11-14 11:08:55 +0200

    Drop unused stuff from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,32 +4,10 @@ module.exports = function(grunt) {
 
         qunit: { // internal task or name of a plugin (like "qunit")
             all: ['js/tests/*.html']
-        },
-        watch: {
-            files: [
-                'js/tests/*.js',
-                'js/tests/*.html',
-                'tmpl/*.html',
-                'js/*.js',
-                'src/*.go'
-            ],
-            tasks: ['qunit', 'shell:buildGo', 'shell:testGo']
-        },
-        shell: {
-            buildGo: {
-                command: 'make gobuild',
-                options: {stdout: true, stderr: true}
-            },
-            testGo: {
-                command: 'make gotest',
-                options: {stdout: true, stderr: true}
-            }
         }
     });
     // load up your plugins
     grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-shell');
     // register one or more task lists (you should ALWAYS have a "default" task list)
 
     // Disable qunit for now, I can't get it working within docker easily:
